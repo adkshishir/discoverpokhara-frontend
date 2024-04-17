33fbf9edf4e3b5da335a9283a6da253a23b6737b
Shishir Adhikari <98789290+adkshishir@users.noreply.github.com>
2024-04-18 01:00:46 +0545

    Fix Post Page Issue

--- src/components/Tags.svelte
+++ src/components/Tags.svelte
@@ -2,22 +2,28 @@
 <script lang="ts">
 	import { FRONTEND_URL, TAGS } from "$lib/const/api";
 	import getApi from "$lib/helper/get";
+	import { error } from "@sveltejs/kit";
 	import { onMount } from "svelte";
+	
+     let tags: { category:{slug:string};title: string; slug: string }[];
 
-    export let tags: { category:{slug:string};name: string; slug: string }[] = [];
        async function fetchData() {
         let data = await getApi(TAGS);
         tags = data.tags;
        }
        onMount(async()=>{
       await fetchData();
+    //   console.log(tags)
        })
 </script>
 
 <!-- Tags Start -->
         <div class="d-flex flex-wrap m-n1">
+           
+            {#if tags?.length > 0}
             {#each tags as tag}
-            <a data-sveltekit-reload href="{FRONTEND_URL}/{tag.category?.slug}/{tag?.slug}" class="btn btn-sm btn-outline-secondary m-1">{tag.name}</a>
+            <a data-sveltekit-reload href="{FRONTEND_URL}/{tag.category?.slug}/{tag?.slug}" class="btn btn-sm btn-outline-secondary m-1">{tag.title}</a>
             {/each}
+            {/if}
         </div>
 <!-- Tags End -->
